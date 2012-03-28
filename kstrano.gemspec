81ba85696e5977fb08f14b76d5c2668c4bf98930
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2012-03-28 10:22:56 +0200

    Remove jenkins and campfire integration, switch to a pull version

--- kstrano.gemspec
+++ kstrano.gemspec
@@ -1,24 +1,24 @@
 Gem::Specification.new do |s|
   s.name        = 'kstrano'
-  s.version     = '0.0.1'
+  s.version     = '0.0.2'
   s.summary     = <<-DESC.strip.gsub(/\n\s+/, " ")
-    Deploying Kunstmaan applications with Capistrano, Jenkins and GIT.
+    Deploying Kunstmaan applications with Capistrano and GIT.
   DESC
   s.description = <<-DESC.strip.gsub(/\n\s+/, " ")
-    Deploying Kunstmaan applications with Capistrano, Jenkins and GIT.
+    Deploying Kunstmaan applications with Capistrano and GIT.
   DESC
 
   s.files       = Dir.glob("lib/**/*")
   s.require_path = 'lib'
   s.has_rdoc    = false
-  
+
   s.bindir = "bin"
   s.executables << "kumafy"
-  
+
   s.author      = "Kunstmaan"
   s.email       = 'hello@kunstmaan.be'
   s.homepage    = 'http://www.kunstmaan.be'
-  
+
   s.add_dependency(%q<capistrano>, [">= 2.9.0"])
   s.add_dependency(%q<json>, [">= 1.6.1"])
   s.add_dependency(%q<broach>, [">= 0.2.1"])
--- lib/kstrano.rb
+++ lib/kstrano.rb
@@ -1,38 +1,28 @@
-set :jenkins_base_uri, "http://jenkins.uranus.kunstmaan.be/jenkins" 
-set :jenkins_base_job_name, "Default"
-set :jenkins_poll_timeout, 300
-set :jenkins_poll_interval, 2
-
-set :campfire_room, nil
-set :campfire_token, "3b2b697bb5ebd879f00cb5cf7ebe1d3b5377768e" ## kbot
-set :campfire_account, "Kunstmaan"
 
 set :airbrake_api_key, nil
 
 require "#{File.dirname(__FILE__)}/helpers/git_helper.rb"
-require "#{File.dirname(__FILE__)}/helpers/jenkins_helper.rb"
 require "#{File.dirname(__FILE__)}/helpers/airbrake_helper.rb"
-require "#{File.dirname(__FILE__)}/helpers/campfire_helper.rb"  
-require "#{File.dirname(__FILE__)}/helpers/kuma_helper.rb"  
+require "#{File.dirname(__FILE__)}/helpers/kuma_helper.rb"
 require 'rexml/document'
 require 'etc'
 
 namespace :kuma do
-  
+
   desc "Run fixcron for the current project"
   task :fixcron do
     sudo "sh -c 'if [ -f /opt/kDeploy/tools/fixcron.py ] ; then cd /opt/kDeploy/tools/; python fixcron.py #{application}; fi'"
   end
-  
+
   desc "Run fixperms for the current project"
   task :fixperms do
     sudo "sh -c 'if [ -f /opt/kDeploy/tools/fixperms.py ] ; then cd /opt/kDeploy/tools/; python fixperms.py #{application}; fi'"
   end
-  
+
 end
 
 namespace :airbrake do
-  
+
   desc "Register a deploy with airbrake.io"
   task :notify do
     if !airbrake_api_key.nil?
@@ -43,94 +33,11 @@ namespace :airbrake do
       Kumastrano.say "Failed notifying airbrake of the new deploy" unless success
     end
   end
-  
-end
 
-namespace :campfire do
-  
-  desc  "Say something on campfire"
-  task:say do
-    if !campfire_room.nil?
-      message = ARGV.join(' ').gsub('campfire:say', '')
-      Kumastrano::CampfireHelper.speak campfire_account, campfire_token, campfire_room, message
-      exit
-    end
-  end
-  
-end
-
-namespace :jenkins do
-  
-  desc "create a job for the current branch and application on Jenkins"
-  task:create_job do
-    ## 1. locate the job in jenkins
-    current_branch = Kumastrano::GitHelper.branch_name
-    job_name = Kumastrano::JenkinsHelper.make_safe_job_name(application, current_branch)
-    current_job_url = Kumastrano::JenkinsHelper.job_url_for_name(jenkins_base_uri, job_name)
-
-    ## 2. if no job was found, first create a job for this branch
-    if current_job_url.nil?
-      Kumastrano.say "no job found for branch #{current_branch} on #{application}, we'll create one now"
-      default_job_url = Kumastrano::JenkinsHelper.job_url_for_name(jenkins_base_uri, jenkins_base_job_name)
-      if !default_job_url.nil?
-        current_refspec = Kumastrano::GitHelper.origin_refspec
-        current_url = Kumastrano::GitHelper.origin_url
-
-        default_job_config = Kumastrano::JenkinsHelper.retrieve_config_xml default_job_url
-        document = REXML::Document.new default_job_config
-        root = document.root
-
-        ## change the description
-        root.elements['description'].text = "This job will be used for testing the branch #{current_branch}"
-
-        ## change the git config
-        git_element = root.elements["scm[@class='hudson.plugins.git.GitSCM']"]
-        git_element.elements["userRemoteConfigs/hudson.plugins.git.UserRemoteConfig/refspec"].text = current_refspec
-        git_element.elements["userRemoteConfigs/hudson.plugins.git.UserRemoteConfig/url"].text = current_url
-        git_element.elements["branches/hudson.plugins.git.BranchSpec/name"].text = current_branch
-
-        ## create the new job based on the modified git config
-        Kumastrano::JenkinsHelper.create_new_job(jenkins_base_uri, job_name, document.to_s)
-        current_job_url = Kumastrano::JenkinsHelper.job_url_for_name(jenkins_base_uri, job_name)
-      end
-    else
-      Kumastrano.say "there was already a job available on Jenkins for branch #{current_branch} on #{application}"
-    end
-    
-    current_job_url
-  end
-  
-  desc "Try to build the current branch on Jenkins"
-  task:build do
-    current_job_url = jenkins::create_job
-    current_branch = Kumastrano::GitHelper.branch_name
-    
-    if !current_job_url.nil?
-      job_name = Kumastrano::JenkinsHelper.make_safe_job_name(application, current_branch)
-      prev_build = Kumastrano::JenkinsHelper.last_build_number current_job_url
-      Kumastrano.say "start building build ##{(prev_build + 1)} on job #{job_name}, this can take a while"
-      
-      result, last_build_info = Kumastrano::JenkinsHelper.build_and_wait current_job_url, jenkins_poll_timeout, jenkins_poll_interval
-
-      message = ""
-      if result
-        Kumastrano.say "the build was succesful"
-        message = "was a success"
-      else
-        Kumastrano.say "the build failed"
-        message = "failed"
-      end
-      
-      Kumastrano::CampfireHelper.speak campfire_account, campfire_token, campfire_room, "#{Etc.getlogin.capitalize} just builded a new version of #{current_branch} on #{application} and it #{message}. You can view the results here #{current_job_url}/lastBuild."
-    else
-      Kumastrano.say "no job found for #{job_name}, cannot build"
-    end
-  end
-  
 end
 
 namespace :deploy do
-  
+
   task :symlink, :except => { :no_release => true } do
       on_rollback do
         if previous_release
@@ -142,122 +49,10 @@ namespace :deploy do
 
       try_sudo "ln -sfT #{latest_release} #{current_path}"
     end
-  
+
 end
 
 ## Capistrano callbacks ##
-
-# Before deploy:
-## Check if there is a build on jenkins. If none is available, it will create one.
-## Notify on Campfire what the deployer did.
-before :deploy do
-  ## only do this in production environment
-  if env == 'production'
-    can_deploy = false
-    current_branch = Kumastrano::GitHelper.branch_name
-    current_hash = Kumastrano::GitHelper.commit_hash
-      
-    if Kumastrano::JenkinsHelper.available? jenkins_base_uri
-      ## Allways fetch the latest information from git
-      Kumastrano::GitHelper.fetch
-  
-      job_name = Kumastrano::JenkinsHelper.make_safe_job_name(application, current_branch)
-      current_job_url = Kumastrano::JenkinsHelper.job_url_for_name(jenkins_base_uri, job_name)  
-
-      if current_job_url.nil?
-        ## No job exists for the current branch, we'll create a job.
-        if Kumastrano.ask "no job found for the current branch, do you want to create a job for #{current_branch} on #{application}?", 'y'
-          current_job_url = jenkins::create_job
-        end
-      end
-
-      if !current_job_url.nil?
-        ## we have a job url, get info of the lastBuild
-        last_build_info = Kumastrano::JenkinsHelper.build_info current_job_url
-        result = last_build_info['result'] ## SUCCESS or FAILURE
-        build_hash = Kumastrano::JenkinsHelper.fetch_build_hash_from_build_info(last_build_info, current_branch)
-  
-        if !build_hash.nil?
-          if build_hash == current_hash
-            if "SUCCESS" == result
-              ## The hash of the last build is the same as my hash we can deploy
-              can_deploy = true
-            else
-              ## The hash of the last build is the same as the current hash, but the build failed.
-              if Kumastrano.ask "the last build for the branch #{current_branch} for commit #{current_hash} failed, do you want to build again?", 'y'
-                prev_build = Kumastrano::JenkinsHelper.last_build_number current_job_url
-                Kumastrano.say "start building build ##{(prev_build + 1)} on job #{job_name}, this can take a while"
-                result, last_build_info = Kumastrano::JenkinsHelper.build_and_wait current_job_url, jenkins_poll_timeout, jenkins_poll_interval
-                new_build_hash = Kumastrano::JenkinsHelper.fetch_build_hash_from_build_info(last_build_info, current_branch)
-                if !result.nil? && result && !new_build_hash.nil? && new_build_hash = current_hash
-                  can_deploy = true
-                else
-                  Kumastrano.say "there is still something wrong with #{current_branch} on #{application}, please check manually and try deploying again afterwards!"
-                end
-              end
-            end
-          else
-            merge_base = Kumastrano::GitHelper.merge_base(build_hash, current_hash)
-            if merge_base == build_hash
-              ## The build commit is an ancestor of HEAD        
-              if Kumastrano.ask "the last build for the branch #{current_branch} is from an older commit do you want to build again? (jenkins=#{build_hash}, local=#{current_hash})", 'y'
-                prev_build = Kumastrano::JenkinsHelper.last_build_number current_job_url
-                Kumastrano.say "start building build ##{(prev_build + 1)} on job #{job_name}, this can take a while"
-                result, last_build_info = Kumastrano::JenkinsHelper.build_and_wait current_job_url, jenkins_poll_timeout, jenkins_poll_interval
-                new_build_hash = Kumastrano::JenkinsHelper.fetch_build_hash_from_build_info(last_build_info, current_branch)
-                if !result.nil? && result && !new_build_hash.nil? && new_build_hash = current_hash
-                  can_deploy = true
-                else
-                  Kumastrano.say "there is still something wrong with #{current_branch} on #{application}, please check manually and try deploying again afterwards!"
-                end
-              end
-            elsif merge_base == current_hash
-              ## The current HEAD is an ancestor of the build hash
-              Kumastrano.say "the latest build is of a newer commit, someone else is probably working on the same branch, try updating your local repository first. (jenkins=#{build_hash}, local=#{current_hash})"
-            else
-              ## Something is wrong, we don't know what try building again
-              if Kumastrano.ask "the latest build isn't a valid build, do you want to try building again? (jenkins=#{build_hash}, local=#{current_hash})", 'y'
-                prev_build = Kumastrano::JenkinsHelper.last_build_number current_job_url
-                Kumastrano.say "start building build ##{(prev_build + 1)} on job #{job_name}, this can take a while"
-                result, last_build_info = Kumastrano::JenkinsHelper.build_and_wait current_job_url, jenkins_poll_timeout, jenkins_poll_interval
-                new_build_hash = Kumastrano::JenkinsHelper.fetch_build_hash_from_build_info(last_build_info, current_branch)
-                if !result.nil? && result && !new_build_hash.nil? && new_build_hash = current_hash
-                  can_deploy = true
-                else
-                  Kumastrano.say "there is still something wrong with #{current_branch} on #{application}, please check manually and try deploying again afterwards!"
-                end
-              end
-            end
-          end
-        else
-          ## No build found, try building it
-          if Kumastrano.ask "no build found, do you want to try building it?", 'y'
-            prev_build = Kumastrano::JenkinsHelper.last_build_number current_job_url
-            Kumastrano.say "start building build ##{(prev_build + 1)} on job #{job_name}, this can take a while"
-            result, last_build_info = Kumastrano::JenkinsHelper.build_and_wait current_job_url, jenkins_poll_timeout, jenkins_poll_interval
-            new_build_hash = Kumastrano::JenkinsHelper.fetch_build_hash_from_build_info(last_build_info, current_branch)
-            if !result.nil? && result && !new_build_hash.nil? && new_build_hash = current_hash
-              can_deploy = true
-            else
-              Kumastrano.say "there is still something wrong with #{current_branch} on #{application}, please check manually and try deploying again afterwards!"
-            end
-          end
-        end
-      end
-    end
-  
-    if !can_deploy
-      if Kumastrano.ask "no valid build found for #{current_hash} on branch #{current_branch}, do you still want to deploy?"
-        Kumastrano::CampfireHelper.speak campfire_account, campfire_token, campfire_room, "#{Etc.getlogin.capitalize} ignored the fact there was something wrong with #{current_branch} on #{application} and still went on with deploying it!!"
-      else
-        Kumastrano::CampfireHelper.speak campfire_account, campfire_token, campfire_room, "#{Etc.getlogin.capitalize} wanted to deploy #{current_branch} on #{application} but there is something wrong with the code, so he cancelled it!"
-        exit
-      end
-    else
-      Kumastrano::CampfireHelper.speak campfire_account, campfire_token, campfire_room, "#{Etc.getlogin.capitalize} is deploying #{current_branch} for #{application}"
-    end
-  end
-end
 
 # Before update_code:
 ## Make the cached_copy readable for the current user
@@ -287,7 +82,6 @@ end
 ## Notify airbrake to add a new deploy to the deploy history
 after :deploy do
   current_branch = Kumastrano::GitHelper.branch_name
-  Kumastrano::CampfireHelper.speak campfire_account, campfire_token, campfire_room, "#{Etc.getlogin.capitalize} successfuly deployed #{current_branch} for #{application}"
   airbrake::notify
   deploy::cleanup ## cleanup old releases
   kuma::fixcron
