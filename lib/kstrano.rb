d8993bdc214786a40dc1fdb3c32b95f32653ef1c
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2012-06-01 22:21:30 +0200

    Clear APC

--- lib/kstrano.rb
+++ lib/kstrano.rb
@@ -305,5 +305,7 @@ after :deploy do
   airbrake::notify
   deploy::cleanup ## cleanup old releases
   kuma::fixcron
-  sudo "/etc/init.d/php5-fpm reload"
+  serverproject = domain.split('.')[0]
+  sudo "echo \"<?php if (in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))) { apc_clear_cache(); apc_clear_cache('user');  apc_clear_cache('opcode');  echo json_encode(array('success' => true)); } else { die('SUPER TOP SECRET'); }\" > /home/projects/#{serverproject}/apcclear.php"
+  sudo "curl http://#{domain}/apcclear.php"
 end
