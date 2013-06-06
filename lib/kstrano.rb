e3738c6b6d6a3af2ad8540ede3ffcaa4ed082f89
Kris Pypen <kris.pypen@kunstmaan.be>
2013-06-06 16:18:52 +0200

    set default port to 22

--- lib/kstrano.rb
+++ lib/kstrano.rb
@@ -9,6 +9,7 @@ set :force_migrations, false
 set :webserver_user,    "www-data"
 set :permission_method, :acl
 set :server_name, nil
+set :port, 22
 
 set :dump_assetic_assets, true
 set :interactive_mode, false
