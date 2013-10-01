8bb3490089a532ae8b04b76579365cf893e8f621
Stéphane Bakelants <stephane.bakelants@kunstmaan.be>
2013-10-01 13:12:19 +0200

    fix indentation

--- lib/kstrano_play.rb
+++ lib/kstrano_play.rb
@@ -8,8 +8,8 @@ module KStrano
         load_paths.push File.expand_path('../', __FILE__)
         load 'kstrano'
 
-         unless exists?(:play_version)
-            set :play_version, "2.2.0"
+        unless exists?(:play_version)
+          set :play_version, "2.2.0"
         end
 
         namespace :deploy do
